823249200be6924b09d6eb2fb043fe09ac7bade1
4bderahman <146900899+4bderahman@users.noreply.github.com>
2023-10-18 15:16:12 +0100

    Create Character-Replacement-in-String.py

---
+++ Character-Replacement-in-String.py
@@ -0,0 +1,6 @@
+string = input("Enter a string: ")
+x = input("Enter a character to replace: ")
+y = input("Enter the character to replace it with: ")
+
+new_string = string.replace(x, y)
+print(new_string)
